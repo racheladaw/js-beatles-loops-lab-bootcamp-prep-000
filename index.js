bc083b075b1746e8ccf7621dd269f489af4547d7
Rachel Williams <williamsrachelada@gmail.com>
2019-01-25 00:25:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var x = []
+  var i
+  for (i = 0; i < 4; i++) {
+    x.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+      return x
+}
+
+function johnLennonFacts (facts) {
+  var i = facts.length
+  var x = 0
+  console.log(i)
+  var z = []
+  while (i > 0){
+    z.push(`${facts[x]}!!!`);
+    x++
+    i--;
+  }
+  return z
+}
+
+function iLoveTheBeatles(num) {
+  var x = []
+  var y = 0
+  do {
+    x.push("I love the Beatles!");
+    y++;
+  }
+  while (num < y < 15)
+  return x
+}
